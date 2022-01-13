c01d5e7a854ac1cfab3567171034a995b4bc6340
r-hatako7814 <ryuichiro.hata@gmail.com>
2022-01-13 21:41:23 +0900

    16まで終了

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
-  public static void main(String.args[]){
-    System.out.println("Hello Worlds");
+  public static void main(String args[]){
+    System.out.println("こんにちは、世界！");
   }
 }
